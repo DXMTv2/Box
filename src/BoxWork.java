b687fc2dfd6a1c6b629134e21a2f472ea72b0275
DXMTv2 <36479870+DXMTv2@users.noreply.github.com>
2018-04-13 09:34:08 -0400

    WordBox

--- src/BoxWork.java
+++ src/BoxWork.java
@@ -4,42 +4,24 @@ public class BoxWork {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        // Make a scanner
+		//By DLYAN & TYRIQUE (April 13th, Friday 2018)
+		
 		Scanner in=new Scanner(System.in);
-		
-		//Make variables
 		String input;
-		
-		//Ask for a string
-		System.out.println("Give me a string");
-		
-		//Read in String
+		System.out.println("Write in a word: ");
 		input=in.nextLine();
-		
-		//PART 1 Top Line
-		//print *, every letter, *
 		System.out.print("* ");
 		for (int x=0;x<input.length();x++) {
 			System.out.print(input.charAt(x)+" ");
 		}
 		System.out.println("*");
-		
-		//PART 2 MIDDLE
-		//Loop fpr every letter in string
 		for (int x=input.length()-1;x>=0;x--) {
-			//print the letters starting at end
 			System.out.print(input.charAt(x)+" ");
-			//loop for every letter in string	
 			for (int y=input.length()-1;y>=0;y--) {
-				//print a star
 				System.out.print("* ");
 			}
-		
-		//print the letters starting at the beginning
 		System.out.println(input.charAt(input.length()-1-x));
 		}
-		//PART 3 BOTTOM LINE
-		//print *, every letter
 		System.out.print("* ");
 		for (int x=input.length()-1;x>=0;x--) {
 			System.out.print(input.charAt(x)+" ");
